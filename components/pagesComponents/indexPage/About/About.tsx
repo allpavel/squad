b465136a3a078db3cef6116bb3353952fc55f407
allpavel <allpavel@yandex.ru>
2022-05-31 20:59:54 +0300

    added hero and about components to the index page

---
+++ components/pagesComponents/indexPage/About/About.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Link from "next/link";
+import { BiReceipt, BiCubeAlt, BiImages, BiShield, BiChevronRight } from "react-icons/bi";
+import styles from "../../../../styles/About.module.css";
+
+const About: React.FC = () => {
+    return (
+        <section id="about" className={styles.about}>
+            <div className={styles.container}>
+                <article className={styles.bigCell}>
+                    <h2>They are life make our spirit</h2>
+                    <p>
+                        The called female you kind under darkness creepeth image cattle green give very sixth was, give
+                        signs a. Bearing made thing seasons above above stars yielding grass let moveth itself.
+                    </p>
+                    <Link href="/about">
+                        <a>
+                            About Us <BiChevronRight />
+                        </a>
+                    </Link>
+                </article>
+                <article className={styles.smallCell}>
+                    <div>
+                        <BiReceipt />
+                        <h2>Together brought fill</h2>
+                        <p>
+                            There moved creeping the to. Creeping years i good fruit fowl doesn&apos;t, his bring a
+                            creepeth him so his.
+                        </p>
+                    </div>
+                    <div>
+                        <BiCubeAlt />
+                        <h2>Gathering one creepeth</h2>
+                        <p>
+                            I is not beginning divided divided be likeness image were. Two Fruit fowl, a sea male his.
+                            Lesser seas was saw.
+                        </p>
+                    </div>
+                    <div>
+                        <BiImages />
+                        <h2>Day herb spirit us he</h2>
+                        <p>
+                            Upon they&apos;re had meat fish winged there wherein for. Their man after multiply signs.
+                            Sea own living good. Lesser there.
+                        </p>
+                    </div>
+                    <div>
+                        <BiShield />
+                        <h2>Land blessed bring above</h2>
+                        <p>
+                            Form behold from light place gathering created whales midst after may divide replenish,
+                            brought multiply whose. Life there. Multiply set.
+                        </p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    );
+};
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from "next";
 import Header from "../components/commonComponents/Header/Header";
+import Hero from "../components/pagesComponents/indexPage/Hero/Hero";
+import About from "../components/pagesComponents/indexPage/About/About";
 
 const Home: NextPage = () => {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            <Hero />
+            <About />
+        </>
+    );
 };
 
 export default Home;

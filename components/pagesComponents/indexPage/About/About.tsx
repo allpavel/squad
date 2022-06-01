b5a8677fba8d1ce99b074641f6ceb2f3ed71dc89
allpavel <allpavel@yandex.ru>
2022-06-01 19:59:28 +0300

    added animations to the hero and about components

--- components/pagesComponents/indexPage/About/About.tsx
+++ components/pagesComponents/indexPage/About/About.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Link from "next/link";
 import { BiReceipt, BiCubeAlt, BiImages, BiShield, BiChevronRight } from "react-icons/bi";
+import { motion } from "framer-motion";
 import styles from "../../../../styles/About.module.css";
 
 const About: React.FC = () => {
     return (
         <section id="about" className={styles.about}>
             <div className={styles.container}>
-                <article className={styles.bigCell}>
+                <motion.article
+                    className={styles.bigCell}
+                    initial={{ y: 100, opacity: 0 }}
+                    whileInView={{ y: 0, transition: { type: "tween", duration: 1.5 }, opacity: 1 }}
+                    viewport={{ once: true }}
+                >
                     <h2>They are life make our spirit</h2>
                     <p>
                         The called female you kind under darkness creepeth image cattle green give very sixth was, give
@@ -18,40 +24,56 @@ const About: React.FC = () => {
                             About Us <BiChevronRight />
                         </a>
                     </Link>
-                </article>
+                </motion.article>
                 <article className={styles.smallCell}>
-                    <div>
+                    <motion.div
+                        initial={{ y: 100, opacity: 0 }}
+                        whileInView={{ y: 0, transition: { type: "tween", duration: 1.5 }, opacity: 1 }}
+                        viewport={{ once: true }}
+                    >
                         <BiReceipt />
                         <h2>Together brought fill</h2>
                         <p>
                             There moved creeping the to. Creeping years i good fruit fowl doesn&apos;t, his bring a
                             creepeth him so his.
                         </p>
-                    </div>
-                    <div>
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 100, opacity: 0 }}
+                        whileInView={{ y: 0, transition: { type: "tween", duration: 1 }, opacity: 1 }}
+                        viewport={{ once: true }}
+                    >
                         <BiCubeAlt />
                         <h2>Gathering one creepeth</h2>
                         <p>
                             I is not beginning divided divided be likeness image were. Two Fruit fowl, a sea male his.
                             Lesser seas was saw.
                         </p>
-                    </div>
-                    <div>
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 100, opacity: 0 }}
+                        whileInView={{ y: 0, transition: { type: "tween", duration: 1.5 }, opacity: 1 }}
+                        viewport={{ once: true }}
+                    >
                         <BiImages />
                         <h2>Day herb spirit us he</h2>
                         <p>
                             Upon they&apos;re had meat fish winged there wherein for. Their man after multiply signs.
                             Sea own living good. Lesser there.
                         </p>
-                    </div>
-                    <div>
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 100, opacity: 0 }}
+                        whileInView={{ y: 0, transition: { type: "tween", duration: 1 }, opacity: 1 }}
+                        viewport={{ once: true }}
+                    >
                         <BiShield />
                         <h2>Land blessed bring above</h2>
                         <p>
                             Form behold from light place gathering created whales midst after may divide replenish,
                             brought multiply whose. Life there. Multiply set.
                         </p>
-                    </div>
+                    </motion.div>
                 </article>
             </div>
         </section>
--- components/pagesComponents/indexPage/Hero/Hero.tsx
+++ components/pagesComponents/indexPage/Hero/Hero.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { BiChevronsDown } from "react-icons/bi";
+import { motion } from "framer-motion";
 import styles from "../../../../styles/Hero.module.css";
 
 const Hero: React.FC = () => {
     return (
         <section className={styles.hero}>
-            <h1>Welcome to Squad</h1>
-            <p>We are team of designers making websites</p>
-            <a href="#about">
-                <BiChevronsDown />
-            </a>
+            <motion.div
+                initial={{ y: 100, opacity: 0 }}
+                whileInView={{ y: 0, transition: { type: "tween", duration: 1 }, opacity: 1 }}
+                viewport={{ once: true }}
+            >
+                <h1>Welcome to Squad</h1>
+                <p>We are team of designers making websites</p>
+                <a href="#about">
+                    <BiChevronsDown />
+                </a>
+            </motion.div>
         </section>
     );
 };

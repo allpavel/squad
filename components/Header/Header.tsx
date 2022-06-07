47b3efefc714640a86faa15658973fadc1ca3f1d
allpavel <allpavel@yandex.ru>
2022-06-07 20:39:12 +0300

    added changes for header background depending on path name

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../Navbar/Navbar";
 import Logo from "../Logo/Logo";
 import styles from "../../styles/Header.module.css";
+import { useRouter } from "next/router";
 
 const Header: React.FC = () => {
+    const [header, setHeader] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        const changeHeaderBackgroundColor = () => {
+            if (window.scrollY >= 80) {
+                setHeader(true);
+            } else {
+                setHeader(false);
+            }
+        };
+
+        window.addEventListener("scroll", changeHeaderBackgroundColor);
+
+        return () => window.removeEventListener("scroll", changeHeaderBackgroundColor);
+    }, [header]);
+
     return (
-        <header className={styles.header} id="header">
-            <Logo />
-            <NavBar />
+        <header
+            className={`${styles.header} ${router.pathname !== "/" ? styles.coloredHeader : ""} ${
+                header ? styles.scrolledHeader : ""
+            }`}
+            id="header"
+        >
+            <div className={styles.container}>
+                <Logo />
+                <NavBar />
+            </div>
         </header>
     );
 };
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,34 +2,37 @@ import React from "react";
 import Link from "next/link";
 import { BiChevronDown } from "react-icons/bi";
 import styles from "../../styles/NavBar.module.css";
+import { useRouter } from "next/router";
 
 const NavBar: React.FC = () => {
+    const router = useRouter();
+
     return (
         <nav className={styles.navigation}>
             <ul className={styles.navigationList}>
                 <li className={styles.navigationListItem}>
                     <Link href="/">
-                        <a>Home</a>
+                        <a className={`${router.pathname === "/" ? styles.active : ""}`}>Home</a>
                     </Link>
                 </li>
                 <li className={styles.navigationListItem}>
                     <Link href="/about">
-                        <a>Abous Us</a>
+                        <a className={`${router.pathname === "/about" ? styles.active : ""}`}>Abous Us</a>
                     </Link>
                 </li>
                 <li className={styles.navigationListItem}>
                     <Link href="/services">
-                        <a>Services</a>
+                        <a className={`${router.pathname === "/services" ? styles.active : ""}`}>Services</a>
                     </Link>
                 </li>
                 <li className={styles.navigationListItem}>
                     <Link href="/portfolio">
-                        <a>Portfolio</a>
+                        <a className={`${router.pathname === "/portfolio" ? styles.active : ""}`}>Portfolio</a>
                     </Link>
                 </li>
                 <li className={styles.navigationListItem}>
                     <Link href="/team">
-                        <a>Team</a>
+                        <a className={`${router.pathname === "/team" ? styles.active : ""}`}>Team</a>
                     </Link>
                 </li>
                 <li className={`${styles.navigationListItem} ${styles.dropdown}`}>

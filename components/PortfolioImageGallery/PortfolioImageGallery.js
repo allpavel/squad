fbffc951f608da0599de862d7a7f6caf16879178
allpavel <allpavel@yandex.ru>
2022-06-28 22:58:36 +0300

    small fixes

--- components/PortfolioImageGallery/PortfolioImageGallery.js
+++ components/PortfolioImageGallery/PortfolioImageGallery.js
@@ -5,7 +5,6 @@ const PortfolioImageGallery = ({ cards, cardIndex }) => {
     const images = cards.map((card) => ({
         original: `http://localhost:1337${card.attributes.image.data[0].attributes.url}`,
     }));
-    console.log("Images: ", images);
     return <ImageGallery items={images} showBullets showPlayButton={false} startIndex={cardIndex} />;
 };
 

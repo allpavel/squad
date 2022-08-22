830c49e72a16475e937e653e3de10fd5ac9fe889
allpavel <allpavel@yandex.ru>
2022-08-22 21:37:55 +0300

    performance improvements on a mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
+import dynamic from "next/dynamic";
 import Head from "next/head";
 import Hero from "../components/Hero/Hero";
 import About from "../components/About/About";
 import Services from "../components/Services/Services";
-import Counter from "../components/Counter/Counter";
-import CallToAction from "../components/CallToAction/CallToAction";
-import Testimonials from "../components/Testimonials/Testimonials";
+
+const Counter = dynamic(() => import("../components/Counter/Counter"));
+const CallToAction = dynamic(() => import("../components/CallToAction/CallToAction"));
+const Testimonials = dynamic(() => import("../components/Testimonials/Testimonials"));
 
 export default function Home() {
     return (
